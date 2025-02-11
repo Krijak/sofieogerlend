83f53a036701e5fcca5a51694324b7e1637fafac
Kristina Jakobsen <kristina.jakobsen@gmail.com>
2025-02-11 17:04:05 +0100

    Routing (#2)
    all pages

---
+++ src/pages/TransportOgOvernatting.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+const TransportOgOvernatting = () => {
+  return <Typography variant="h1">Transport og overnatting</Typography>;
+};
+
+export default TransportOgOvernatting;
