88b8f6167b8d45642bf5f2eabe904d4e8cc97aa7
Kristina Jakobsen <krijak@stud.ntnu.no>
2025-02-15 23:02:28 +0100

    rsvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import { HashRouter } from "react-router-dom";
 import { Route, Routes } from "react-router";
-import {
-  ThemeProvider,
-} from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import Topbar from "./Components/Topbar";
 import Main from "./pages/Main";
 import Fredag from "./pages/Fredag";
 import Loerdag from "./pages/Loerdag";
 import TransportOgOvernatting from "./pages/TransportOgOvernatting";
 import theme from "./theme";
+import Rsvp from "./pages/Rsvp";
+import Submited from "./pages/Submited";
 
 function App() {
   return (
@@ -18,12 +18,14 @@ function App() {
         <Topbar />
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/submited" element={<Submited />} />
           <Route path="/fredag" element={<Fredag />} />
           <Route path="/lørdag" element={<Loerdag />} />
           <Route
             path="/transportogovernatting"
             element={<TransportOgOvernatting />}
           />
+          <Route path="/rsvp" element={<Rsvp />} />
         </Routes>
       </ThemeProvider>
     </HashRouter>
