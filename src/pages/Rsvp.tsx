e63d8baf3caa0058a1d25604a024610f37454235
Kristina Jakobsen <krijak@stud.ntnu.no>
2025-02-25 19:53:23 +0100

    open rsvp in tab

--- src/pages/Rsvp.tsx
+++ src/pages/Rsvp.tsx
@@ -1,15 +1,30 @@
 import FormfacadeEmbed from "@formfacade/embed-react";
+import { Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import PageWrapper from "../Components/PageWrapper";
 
 const Rsvp = () => {
   const navigate = useNavigate();
+  // onSubmitForm={() => navigate("/submited")
   return (
     <PageWrapper>
-      <FormfacadeEmbed
-        formFacadeURL="https://formfacade.com/include/118216848014247750506/form/1FAIpQLSdBsYysWmlnovduxWR2RICMEsIk0uOwYEiuFoqTrgld8U3RZw/classic.js/?div=ff-compose"
-        onSubmitForm={() => navigate("/submited")}
-      />
+      <Box>
+        <FormfacadeEmbed
+          formFacadeURL="https://formfacade.com/include/118216848014247750506/form/1FAIpQLSdBsYysWmlnovduxWR2RICMEsIk0uOwYEiuFoqTrgld8U3RZw/classic.js/?div=ff-compose"
+          onSubmitForm={() => console.log("Form submitted")}
+        />
+
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdBsYysWmlnovduxWR2RICMEsIk0uOwYEiuFoqTrgld8U3RZw/viewform?embedded=true"
+          width="100%"
+          height="100%"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Laster inn …
+        </iframe>
+      </Box>
     </PageWrapper>
   );
 };
