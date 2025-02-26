0285fdf2330975d28966d46abc9fb56122825ace
Kristina Jakobsen <krijak@stud.ntnu.no>
2025-02-26 17:52:27 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Box } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
-import { PropsWithChildren, useEffect, useLayoutEffect, useState } from "react";
-import { Route, Routes, useLocation } from "react-router";
+import { useState } from "react";
+import { Route, Routes } from "react-router";
 import { HashRouter } from "react-router-dom";
+import AppWrapper from "./Components/AppWrapper";
 import Topbar from "./Components/Topbar";
 import Fredag from "./pages/Fredag";
 import Informasjon from "./pages/Informasjon";
@@ -12,94 +12,20 @@ import Main from "./pages/Main";
 import TransportOgOvernatting from "./pages/TransportOgOvernatting";
 import theme from "./theme";
 
-const Wrapper = ({ children }: PropsWithChildren) => {
-  const location = useLocation();
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("scroll-animation");
-      }
-    });
-  });
-
-  useLayoutEffect(() => {
-    const viewbox = document.querySelectorAll(".apply-scroll-animation");
-    viewbox.forEach((element) => {
-      observer.observe(element);
-    });
-    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
-  }, [location.pathname]);
-
-  return children;
-};
-
-const preloadImages = (images: string[]): Promise<void[]> => {
-  const imagePromises = images.map((src) => {
-    return new Promise<void>((resolve, reject) => {
-      const img = new Image();
-      img.src = src;
-      img.onload = () => resolve();
-      img.onerror = () => reject(`Failed to load image: ${src}`);
-    });
-  });
-
-  return Promise.all(imagePromises);
-};
-
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [showLoading, setShowLoading] = useState(true);
-  useEffect(() => {
-    const images: string[] = [
-      ...Object.values(
-        import.meta.glob("/public/*.{png,jpg,jpeg,gif}", { eager: true })
-      ).map((module) => (module as any).default),
-      ...Object.values(
-        import.meta.glob("/public/SofieErlend/*.{png,jpg,jpeg}", {
-          eager: true,
-        })
-      ).map((module) => (module as any).default),
-    ];
-
-    preloadImages(images)
-      .then(() => {
-        setTimeout(() => {
-          setShowLoading(false);
-          setTimeout(() => {
-            setIsLoaded(true);
-          }, 500);
-        }, 500);
-      })
-      .catch(() => {
-        setShowLoading(false);
-        setIsLoaded(true);
-      });
-  }, [location.pathname]);
 
   return (
     <HashRouter basename={import.meta.env.BASE_URL}>
-      <Wrapper>
+      <AppWrapper setIsLoaded={setIsLoaded} setShowLoading={setShowLoading}>
         <ThemeProvider theme={theme}>
           {!isLoaded && <Loading isLoading={showLoading} />}
           <Topbar />
           {isLoaded && (
             <Routes>
-              <Route
-                path="/"
-                element={
-                  <Box className="scroll-animation">
-                    <Main />
-                  </Box>
-                }
-              />
-              <Route
-                path="/main"
-                element={
-                  <Box className="scroll-animation">
-                    <Main />
-                  </Box>
-                }
-              />
+              <Route path="/" element={<Main />} />
+              <Route path="/main" element={<Main />} />
               <Route path="/fredag" element={<Fredag />} />
               <Route path="/lørdag" element={<Loerdag />} />
               <Route path="/informasjon" element={<Informasjon />} />
@@ -110,7 +36,7 @@ function App() {
             </Routes>
           )}
         </ThemeProvider>
-      </Wrapper>
+      </AppWrapper>
     </HashRouter>
   );
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,38 +6,40 @@ import AnimatedImage from "../Components/AnimatedImage";
 import PageWrapper from "../Components/PageWrapper";
 
 const images: string[] = Object.values(
-  import.meta.glob("/public/SofieErlend/*.{png,jpg,jpeg}", { eager: true })
+  import.meta.glob("/public/FrontpageImages/*.{png,jpg,jpeg}", { eager: true })
 ).map((module) => (module as any).default);
 
 const Main = () => {
   const location = useLocation();
   const randomImageIndex = Math.floor(Math.random() * images.length);
+  const alt = "Sofie + Erlend";
 
   return (
-    <PageWrapper pt={1}>
-      <Box width={{ xs: "80px", sm: "100px" }}>
-        <img src={SE} alt="Sofie + Erlend" width={"100%"} />
-      </Box>
+    <Box className="scroll-animation">
+      <PageWrapper pt={1}>
+        <Box width={{ xs: "80px", sm: "100px" }}>
+          <img src={SE} alt={alt} width={"100%"} />
+        </Box>
 
-      <StyledNavLink to={location.pathname == "/" ? "/main" : "/"}>
-        <AnimatedImage
-          src={images[randomImageIndex]}
-          key={randomImageIndex}
-          alt="Sofie og Erlend"
-          className="fade-image"
-          style={{
-            maxWidth: "500px",
-            minWidth: "100px",
-          }}
-          padding={8}
-          paddingBottom={{ xs: 2, sm: 6 }}
-          paddingTop={{ xs: 2, sm: 4 }}
-        />
-      </StyledNavLink>
-      <Box width={{ xs: "200px", sm: "250px" }}>
-        <img src={SofieOgErlend} alt="Sofie og Erlend" width={"100%"} />
-      </Box>
-    </PageWrapper>
+        <StyledNavLink to={location.pathname == "/" ? "/main" : "/"}>
+          <AnimatedImage
+            src={images[randomImageIndex]}
+            key={randomImageIndex}
+            alt={alt}
+            style={{
+              maxWidth: "500px",
+              minWidth: "100px",
+            }}
+            padding={8}
+            paddingBottom={{ xs: 2, sm: 6 }}
+            paddingTop={{ xs: 2, sm: 4 }}
+          />
+        </StyledNavLink>
+        <Box width={{ xs: "200px", sm: "250px" }}>
+          <img src={SofieOgErlend} alt={alt} width={"100%"} />
+        </Box>
+      </PageWrapper>
+    </Box>
   );
 };
 
